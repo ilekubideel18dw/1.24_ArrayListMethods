265ed449147513e9bf5ca80e90d6e7c0034ef205
ik013043z1 <ik013043z1@H013043.NET>
2018-11-07 08:47:09 +0100

    2.0

--- src/ArrayListMethods.java
+++ src/ArrayListMethods.java
@@ -1,63 +1,87 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListMethods {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		// Create a String ArrayList.
 		ArrayList<String> cars = new ArrayList<String>();
 		
 		// Enter elements.
-		cars.add("Fiat");
-		cars.add("Volvo");
-		cars.add("Opel");
-		cars.add("Seat");
-		cars.add("Nissan");
-		cars.add("Citroen");
-		cars.add("Seat");
+		System.out.println("Enter 5 cars for our Rallysprint:");
+		cars.add(sc.next());
+		cars.add(sc.next());
+		cars.add(sc.next());
+		cars.add(sc.next());
+		cars.add(sc.next());
 		
 		// See the ArrayList content.
 		System.out.println(cars);
 		
 		// Enter a new element in the third position.
-		cars.add(3, "Mercedes");
+		System.out.println("Enter a position and then enter a new car for this position:");
+		cars.add(sc.nextInt(), sc.next());
 		
 		// See the ArrayList content.
 		System.out.println(cars);
 		
 		// See the size of the ArrayList.
-		System.out.println("We have " + cars.size() + " cars in our parking.");
+		System.out.println("We have " + cars.size() + " cars in our rallysprint.");
 		
 		// See the element of the second position.
-		System.out.println("The car of the second position is a " + cars.get(2));
+		System.out.println("What positions car do you want to see?");
+		System.out.println("The car of that position is a " + cars.get(sc.nextInt()));
 		
 		// See if the element is in the list or not with a boolean result.
-		System.out.println("Is any Fiat in the parking? " + cars.contains("Fiat"));
+		System.out.println("Enter a car brand to see its any car of that brand in the rallysprint:");
+		System.out.println(cars.contains(sc.next()));
 		
-		// See the index of the first position of the specified element in this list 
-		System.out.println("The first Seat is in the position number " + cars.indexOf("Seat"));
+		// See the index of a position of the specified element in this list. If the element doesn't exists, the result is -1. 
+		System.out.println("Enter a brand to see what is the first position a car with that brand is:");
+		System.out.println("The first car with that brand is on the number " + cars.indexOf(sc.next()) + " position.");
 		
-		// It is not any Ferrari, so the result is -1.
-		System.out.println("The first Ferrari is in the position number " + cars.indexOf("Ferrari"));
+		// See the index of the last position of the specified element in this list. If the element doesn't exists, the result is -1.
+		System.out.println("Enter a brand to see what is the last position a car with that brand is:");
+		System.out.println("The last car with that brand is on the number " + cars.lastIndexOf(sc.next()) + " position.");
 		
-		// See the index of the last position of the specified element in this list.
-		System.out.println("The last Seat is in the position number" + cars.lastIndexOf("Seat"));
+		// Delete the element of any position.
+		System.out.println("Enter a position to delete the car of that position:");
+		cars.remove(sc.nextInt()); 
 		
-		// It is not any Ferrari, so the result is -1.
-		System.out.println("The last Ferrari is in the position number" + cars.lastIndexOf("Ferrari"));
+		// See the ArrayList content.
+		System.out.println(cars);
 		
-		/*
-		nombreArrayList.remove(5); 
+		// Delete the first specified element we enter.
+		System.out.println("Enter a car to delete the first car with that brand:");
+		cars.remove(sc.next());
 		
-		nombreArrayList.remove("Elemento");
+		// See the ArrayList content.
+		System.out.println(cars);
 		
-		nombreArrayList.clear();
+		// Clone the list
+		ArrayList carsCopy = (ArrayList) cars.clone();
+		System.out.println("We create a copy of the list. This is the list:");
 		
-		nombreArrayList.isEmpty();  
+		// See the cloned ArrayList content.
+		System.out.println(carsCopy);
 		
-		ArrayList arrayListCopia = (ArrayList) nombreArrayList.clone();  
+		// Delete all the elements
+		cars.clear();
+		System.out.println("We have a copy, so now we are going to clean the list. This is the result:");
 		
-		Object[] array = nombreArrayList.toArray(); 
-		*/
+		// See the ArrayList content.
+		System.out.println(cars);
+				
+		// See the list is empty or not with boolean result.
+		System.out.println("Is the list empty? " + cars.isEmpty());  
+		
+		// Transform the ArrayList in an array.
+		Object[] array = cars.toArray(); 
+		
+		System.out.println("Thats all. Bye!");
+
 	}
 }
